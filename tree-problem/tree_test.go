08435bbb2186771af80751f6bdf2b6ab11a4f56d
Abrar-Ahmed7 <abrarahmedid@gmail.com>
2022-11-25 09:35:39 +0530

    test-case for json and xml

--- tree-problem/tree_test.go
+++ tree-problem/tree_test.go
@@ -36,7 +36,9 @@ func TestTree(t *testing.T) {
 				"│──sample1.txt\n" +
 				"└──test-folder1\n" +
 				"   └──test-folder2\n" +
-				"      └──sample.txt\n",
+				"      └──sample.txt\n" +
+				"\n" +
+				"3 directories, 2 files",
 		},
 		{
 			dirPath: "./resources",
@@ -44,7 +46,9 @@ func TestTree(t *testing.T) {
 			r:       report{},
 			want: "resources\n" +
 				"└──test-folder1\n" +
-				"   └──test-folder2\n",
+				"   └──test-folder2\n" +
+				"\n" +
+				"2 directories",
 		},
 		{
 			dirPath: "./resources",
@@ -52,12 +56,15 @@ func TestTree(t *testing.T) {
 			r:       report{},
 			want: "resources\n" +
 				"│──sample1.txt\n" +
-				"└──test-folder1\n",
+				"└──test-folder1\n" +
+				"\n" +
+				"2 directories, 1 files",
 		},
 	}
 
 	for _, tc := range tests {
-		got, _ := tree(tc.dirPath, "", "", "", &tc.r, tc.input, 0)
+		tree, _ := tree(tc.dirPath, "", "", "", &tc.r, tc.input, 0)
+		got := appendReport(tree, tc.r, tc.input)
 		if !reflect.DeepEqual(tc.want, got) {
 			t.Fatalf("expected:\n %v, got:\n %v", tc.want, got)
 		}
@@ -65,4 +72,169 @@ func TestTree(t *testing.T) {
 
 	// deleting the created dir
 	os.RemoveAll("./resources/")
+}
+
+func TestJsonTree(t *testing.T) {
+	// creating dir
+	if err := os.MkdirAll("resources/test-folder1/test-folder2", os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+	_, err1 := os.Create("resources/test-folder1/test-folder2/sample.txt")
+	if err1 != nil {
+		fmt.Printf("Unable to open file: %s", err1)
+	}
+	_, err2 := os.Create("resources/sample1.txt")
+	if err2 != nil {
+		fmt.Printf("Unable to open file: %s", err2)
+	}
+
+	tests := []struct {
+		dirPath string
+		input   configs
+		r       report
+		want    string
+	}{
+		{
+			dirPath: "./resources",
+			input:   configs{json: true},
+			r:       report{},
+			want: "[\n" +
+				"  {\"type\":\"directory\",\"name\":\"resources\",\"contents\":[\n" +
+				"    {\"type\":\"file\",\"name\":\"sample1.txt\"},\n" +
+				"    {\"type\":\"directory\",\"name\":\"test-folder1\",\"contents\":[\n" +
+				"      {\"type\":\"directory\",\"name\":\"test-folder2\",\"contents\":[\n" +
+				"        {\"type\":\"file\",\"name\":\"sample.txt\"}\n" +
+				"      ]}\n" +
+				"    ]}\n" +
+				"  ]}\n" +
+				",\n" +
+				"  {\"type\":\"report\",\"directories\":3, \"files\":2}\n" +
+				"]",
+		},
+		// {
+		// 	dirPath: "./resources",
+		// 	input:   configs{dirOnly: true},
+		// 	r:       report{},
+		// 	want: "[\n" +
+		// 	"  {\"type\":\"directory\",\"name\":\"resources\",\"contents\":[\n" +
+		// 	"    {\"type\":\"file\",\"name\":\"sample1.txt\"},\n" +
+		// 	"    {\"type\":\"directory\",\"name\":\"test-folder1\",\"contents\":[\n" +
+		// 	"      {\"type\":\"directory\",\"name\":\"test-folder2\",\"contents\":[\n" +
+		// 	"        {\"type\":\"file\",\"name\":\"sample.txt\"}\n" +
+		// 	"      ]}\n" +
+		// 	"    ]}\n" +
+		// 	"  ]}\n" +
+		// 	",\n" +
+		// 	"  {\"type\":\"report\",\"directories\":2, \"files\":2}\n" +
+		// 	"]",
+		// },
+		{
+			dirPath: "./resources",
+			input:   configs{json: true, level: 1},
+			r:       report{},
+			want: "[\n" +
+				"  {\"type\":\"directory\",\"name\":\"resources\",\"contents\":[\n" +
+				"    {\"type\":\"file\",\"name\":\"sample1.txt\"},\n" +
+				"    {\"type\":\"directory\",\"name\":\"test-folder1\"  ]}\n" +
+				",\n" +
+				"  {\"type\":\"report\",\"directories\":2, \"files\":1}\n" +
+				"]",
+		},
+	}
+
+	for _, tc := range tests {
+		tree, _ := jsonTree(tc.dirPath, "  ", "", &tc.r, tc.input, 1)
+		got := addSqBrkt(appendReport(tree, tc.r, tc.input))
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("expected:\n %v, got:\n %v", tc.want, got)
+		}
+	}
+
+	// deleting the created dir
+	os.RemoveAll("./resources/")
+
+}
+
+func TestXmlTree(t *testing.T) {
+	// creating dir
+	if err := os.MkdirAll("resources/test-folder1/test-folder2", os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+	_, err1 := os.Create("resources/test-folder1/test-folder2/sample.txt")
+	if err1 != nil {
+		fmt.Printf("Unable to open file: %s", err1)
+	}
+	_, err2 := os.Create("resources/sample1.txt")
+	if err2 != nil {
+		fmt.Printf("Unable to open file: %s", err2)
+	}
+
+	tests := []struct {
+		dirPath string
+		input   configs
+		r       report
+		want    string
+	}{
+		{
+			dirPath: "./resources",
+			input:   configs{xml: true},
+			r:       report{},
+			want: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				"<tree>\n" +
+				"  <directory name=\"resources\">\n" +
+				"	<file name=\"sample1.txt\"></file>\n" +
+				"	<directory name=\"test-folder1\">\n" +
+				"	  <directory name=\"test-folder2\">\n" +
+				"		<file name=\"sample.txt\"></file>\n" +
+				"	  </directory>\n" +
+				"	</directory>\n" +
+				"  </directory>\n" +
+				"  <report>\n    <directories>" +
+				"3</directories>\n	<files>2</files>" +
+				"\n  </report>" +
+				"\n</tree>",
+		},
+		{
+			dirPath: "./resources",
+			input:   configs{xml: true, dirOnly: true},
+			r:       report{},
+			want: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				"<tree>\n" +
+				"  <directory name=\"resources\">\n" +
+				"	<directory name=\"test-folder1\">\n" +
+				"	  <directory name=\"test-folder2\">\n" +
+				"	  </directory>\n" +
+				"	</directory>\n" +
+				"  <report>\n    <directories>" +
+				"2</directories>" +
+				"\n  </report>" +
+				"\n</tree>",
+		},
+		{
+			dirPath: "./resources",
+			input:   configs{xml: true, level: 1},
+			r:       report{},
+			want: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				"<tree>\n" +
+				"  <directory name=\"resources\">\n" +
+				"	<file name=\"sample1.txt\"></file>\n" +
+				"	<directory name=\"test-folder1\">  </directory>" +
+				"  <report>\n    <directories>" +
+				"2</directories>\n	<files>1</files>" +
+				"\n  </report>" +
+				"\n</tree>",
+		},
+	}
+
+	for _, tc := range tests {
+		tree, _ := xmlTree(tc.dirPath, "  ", "", &tc.r, tc.input, 1)
+		got := addTreeTag(appendReport(tree, tc.r, tc.input))
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("expected:\n %v, got:\n %v", tc.want, got)
+		}
+	}
+
+	// deleting the created dir
+	os.RemoveAll("./resources/")
+
 }
